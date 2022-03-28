6b05acac36ffcba43e43c73932368b96aa0c60a9
cloudedpanther <76900250+cloudedpanther@users.noreply.github.com>
2022-03-28 18:01:44 +0900

    Set up schemas (#1)
    * Set up schemas
    
    Set up user and class schema
    Modified directory name: schemas -> models
    Removed file index.js in directory models
    
    * Modified User model
    
    1. Changed field name: schoolId -> userId
    
    2. Modifired email as a required field, and also moved to ouside of meta filed
       (Decided to use email as login ID)
    
    3. Added password filed
    
    * Modified Class model
    
    Changed field name: teacher -> professor
    
    * Added password hashing process
    
    Used bcrypt
    
    Set salt 5

---
+++ src/models/Class.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const classSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    classId: { type: String, required: true },
+    openingTime: { type: String, required: true },
+    closingTime: { type: String, required: true },
+    professor: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    students: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+});
+
+// _id : PK (indexing -> mongoDB 자동 할당)
+// classId : string (X033-2 이런 거)
+// name : string (강의명)
+// openingTime : string(임시) (강의 시작하는 시간)
+// closingTime : string(임시) (강의 끝나는 시간)
+// professor: ObjectId (담당교수 -> User DB에서 _id를 바탕으로 정보를 참조해옴)
+// students: array of ObjectId (담당교수 -> User DB에서 _id를 바탕으로 정보를 참조해옴)
+
+const Class = mongoose.model('Class', classSchema);
+export default Class;
---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    userType: {
+        type: String,
+        enum: ['student', 'professor'],
+        required: true,
+    },
+    userId: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    classes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Class',
+        },
+    ],
+    meta: {
+        phone: { type: String },
+    },
+});
+
+// _id: PK (index용 ID -> mongoDB가 자동 부여)
+// userType: Enum (유저 타입 (교수인지, 학생인지))
+// userId: number (학번 or 교번)
+// name: string (사람이름)
+// email: string (이메일, 로그인시 아이디로 사용)
+// password: string (로그인시 사용하는 비밀번호)
+// class: array of objectid (수강하는 과목들 -> Class DB에서 _id를 바탕으로 정보를 참조해옴)
+// meta: 부가적인 정보
+//      phone : string (전화번호)
+
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 5);
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
