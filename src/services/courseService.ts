4ac418d0fe2b448178de5facbb5b9a6de03c8f27
heroleggo <h3r0.y0u@gmail.com>
2022-05-18 01:59:52 +0900

    :construction: WIP
    - Course 관련 로직 추가
    - 이외 수정 진행중
    - 세션 - socket.io 연결 테스트 예정

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,5 +1,17 @@
-export const getCourses = () => {};
+import { courseRepository } from '~/repositories';
 
-export const getMyCourses = () => {};
+export const getCourses = () => {
+    return courseRepository.find({
+        relations: {
+            professor: true,
+        },
+    });
+};
 
-export const getCourseByClassId = () => {};
+export const getMyCourses = (id: number) => {
+    return courseRepository.findBy({ students: { id } });
+};
+
+export const getCourseByClassId = (classId: string) => {
+    return courseRepository.findOneBy({ classId });
+};
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,15 +1,12 @@
 import { JoinRequest } from '~/dto';
 import { ConflictError } from '~/utils';
-import { dataSource } from '~/config';
-import { User } from '~/models';
+import { userRepository } from '~/repositories';
 
 export const join = async (dto: JoinRequest) => {
     const user = dto.toEntity();
-    const isExist = await dataSource.manager.findOneBy(User, {
-        email: user.email,
-    });
+    const isExist = await userRepository.countBy({ email: user.email });
     if (isExist) {
         throw new ConflictError('Info already exists');
     }
-    return dataSource.manager.save(user);
+    return userRepository.save(user);
 };
