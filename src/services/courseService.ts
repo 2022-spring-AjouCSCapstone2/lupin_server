c5f1262d6c254b808c241bf70192f8e51c0364e3
Youngwoong You <42253726+heroleggo@users.noreply.github.com>
2022-05-24 04:24:51 +0900

    :boom: added timetable on course (#6)
    - 시간표 schema 생성
    - 현재 샘플 데이터가 없어 임시로 데이터 생성 예정
    - 추후 문자열로 이루어진 시간 정보를 객체로 변환하는 script 작성해서 적용 예정

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,17 +1,63 @@
 import { courseRepository } from '~/repositories';
+import { day } from '~/config';
 
 export const getCourses = () => {
     return courseRepository.find({
-        relations: {
-            professor: true,
-        },
+        relations: ['professor', 'timetables'],
     });
 };
 
 export const getMyCourses = (id: number) => {
-    return courseRepository.findBy({ students: { id } });
+    return courseRepository.find({
+        where: {
+            students: {
+                id,
+            },
+        },
+        relations: ['professor', 'timetables'],
+    });
 };
 
 export const getCourseByCourseId = (courseId: string) => {
-    return courseRepository.findOneBy({ courseId });
+    return courseRepository.findOne({
+        where: {
+            courseId,
+        },
+        relations: ['professor', 'timetables'],
+    });
+};
+
+export const getTodayCourses = (id: number) => {
+    const today = new Date().getDay();
+    let dayParam;
+    switch (today) {
+        case 0:
+            dayParam = 'SUNDAY';
+            break;
+        case 1:
+            dayParam = 'MONDAY';
+            break;
+        case 2:
+            dayParam = 'TUESDAY';
+            break;
+        case 3:
+            dayParam = 'WEDNESDAY';
+            break;
+        case 4:
+            dayParam = 'THURSDAY';
+            break;
+        case 5:
+            dayParam = 'FRIDAY';
+            break;
+        case 6:
+            dayParam = 'SATURDAY';
+            break;
+        default:
+            dayParam = 'NODAY';
+            break;
+    }
+    return courseRepository.find({
+        where: { students: { id }, timetables: { day: day[dayParam] } },
+        relations: ['professor', 'timetables'],
+    });
 };
