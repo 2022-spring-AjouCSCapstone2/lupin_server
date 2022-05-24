e213dfbc0676709e889cc1a264bada083f6a2c7e
heroleggo <h3r0.y0u@gmail.com>
2022-05-24 19:40:42 +0900

    :bug: fixed bug
    - CORS 설정 수정 (credentials 옵션)

--- index.ts
+++ index.ts
@@ -32,7 +32,7 @@ const io = new Server(server, { cookie: true });
 
 app.use(
     cors({
-        origin: '*',
+        origin: ['http://localhost:3000', '*'],
         credentials: true,
     }),
 );
